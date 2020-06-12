8e6a89ed65e372238ec12c66bb0879f69d100a67
Vitalii <vitali.poponov@waveaccess.ru>
2020-06-13 00:50:32 +0300

    work on bluetooth service

--- AR-pylos/Core/Network/BluetoothNetworkAdapter.swift
+++ AR-pylos/Core/Network/BluetoothNetworkAdapter.swift
@@ -9,18 +9,31 @@
 
 //Low level interface to find and handle connection
 import Foundation
+import CoreBluetooth
+import RxBluetoothKit
 
+extension BluetoothNetworkAdapter {
+    struct Constants {
+        static let dataServiceUUID = CBUUID(string: "6E6B5C64-FAF7-40AE-9C21-D4933AF45B23")
+    }
+}
 class BluetoothNetworkAdapter: CommunicatorAdapter {
     
     private let disposeBag = DisposeBag()
-
+    private var centralManager = CentralManager(queue: .main)
+    private var peripheralManager = PeripheralManager(queue: .main)
     var outMessages: PublishRelay<Data> = PublishRelay<Data>() //Messages to send to others
     var inMessages: PublishSubject<Data> = PublishSubject<Data>() //Messages received from others
-    
-    func findMatch() -> Observable<Bool> {
-        //DO NOT CHANGE BLUETOOTH YET
-        return Observable.create { (observer) -> Disposable in
-            observer.onNext(true)
+    deinit {
+        print("")
+    }
+    func findMatch() -> Single<Bool> {
+        return Single.create { (observer) -> Disposable in
+            self.peripheralManager.observeState().filter({ $0 == .poweredOn }).flatMap({ _ in
+                self.peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey: [Constants.dataServiceUUID]]) }).flatMap({ _ in self.centralManager.observeState() }).filter({ $0 == .poweredOn }).flatMap({ _ in self.centralManager.scanForPeripherals(withServices: [Constants.dataServiceUUID]) }).subscribe { (event) in
+                    print("")
+            }.disposed(by: self.disposeBag)
+
             return Disposables.create {}
         }
     }
